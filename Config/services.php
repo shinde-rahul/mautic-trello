06e94521d986aaf71598ae36f254fd36e6b67de7
Rahul Shinde <shinde.r.a@gmail.com>
2024-10-31 15:49:24 +0530

    Added Autowiring

---
+++ Config/services.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Mautic\CoreBundle\DependencyInjection\MauticCoreExtension;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $configurator) {
+    $services = $configurator->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $excludes = [];
+    $services->load('MauticPlugin\\MauticTrelloBundle\\', '../')
+        ->exclude('../{'.implode(',', array_merge(MauticCoreExtension::DEFAULT_EXCLUDES, $excludes)).'}');
+};
